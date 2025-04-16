de9ed087e9a047e95c149211fb234039f86e0a7c
Dev713 <53387865+erikg713@users.noreply.github.com>
2025-04-15 23:36:51 -0500

    Update autotile.js

--- Game/utils/autotile.js
+++ Game/utils/autotile.js
@@ -1,1 +1,8 @@
+function autoTile(tileType, neighbors) {
+    // Logic for autotiling based on neighboring tiles
+}
+
+function createAnimation(frames, frameRate, loop = true) {
+    return { frames, frameRate, loop };
+}
 
