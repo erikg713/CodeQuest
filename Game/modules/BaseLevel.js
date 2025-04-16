0a86c78e36668b521d3bea78593e3ca5df389e07
Dev713 <53387865+erikg713@users.noreply.github.com>
2025-04-15 23:36:15 -0500

    Update BaseLevel.js

--- Game/modules/BaseLevel.js
+++ Game/modules/BaseLevel.js
@@ -1,1 +1,19 @@
+class BaseLevel {
+    constructor(metadata, layout, entities, environment) {
+        this.metadata = metadata;
+        this.layout = layout;
+        this.entities = entities;
+        this.environment = environment;
+    }
 
+    loadEntities(entities) {
+        this.entities = entities.map(entity => new Entity(entity.type, entity.position, entity.properties));
+    }
+}
+
+class GrasslandLevel extends BaseLevel {
+    constructor(metadata, layout, entities, environment, theme) {
+        super(metadata, layout, entities, environment);
+        this.theme = theme; // World-specific elements like background music
+    }
+}
